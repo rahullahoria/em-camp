f45d692d4c6c71d4d8f517f503f2b63b64f4f9f1
rahullahoria <rahul_lahoria@yahoo.com>
2016-04-22 21:22:41 +0530

    crowl upto level 3

--- urlFinder.py
+++ urlFinder.py
@@ -5,23 +5,35 @@ import urllib2
 import time
 
 ####################################
-def savePage( url ):
-    headers = {'USER-Agent':'crawltaosof'}
-    req = urllib2.Request(url, None,headers)
-    try:
-        pageObj = urllib2.urlopen(req,timeout=0.51)
-        print pageObj.info().getheader('Content-Type')
-        if "text/html" in pageObj.info().getheader('Content-Type'):
-            page = pageObj.read()
-            print page
-            f = open("crowl/"+str(time.time())+"_"+url.split("/")[2]+".html","w") #opens file with name of "test.txt"
-            f.write(page)
-            f.close()
-            return 1
-    except Exception, err:
-        print Exception, err
-        return 0
-    return 0
+def getUrls(page):
+	return re.findall("(?P<url>https?://[^\s\"<]+)", page)
+	
+
+
+def savePage( url, level ):
+	print level
+	if(level==3):
+		return
+	headers = {'USER-Agent':'crawltaosof'}
+	req = urllib2.Request(url, None,headers)
+	try:
+		pageObj = urllib2.urlopen(req,timeout=0.51)
+		print pageObj.info().getheader('Content-Type')
+		if "text/html" in pageObj.info().getheader('Content-Type'):
+			page = pageObj.read()
+			ts = getUrls(page)
+			for t in ts:
+				for i in range(0,3):
+					savePage( t, level+i )
+            #print page
+			f = open("crowl/"+str(time.time())+"_"+url.split("/")[2]+".html","w") #opens file with name of "test.txt"
+			f.write(page)
+			f.close()
+			return 1
+	except Exception, err:
+		print Exception, err
+		return 0
+	return 0
 
 
 def uniqueListConc(first_list,second_list):
@@ -31,48 +43,47 @@ def uniqueListConc(first_list,second_list):
     result = first_list + list(in_second_but_not_in_first)
     return result
 
+def getGSLinks():
+	br = mechanize.Browser() #initiating a browser
+	br.set_handle_robots(False) #ignore robots.txt
+	br.addheaders = [("User-agent","Mozilla/5.0")] #our identity in the web
+	qe=""
+
+	alert = ""
+	global q
+	for i in range(0,len(q)):
+		if q[i] ==" ":
+			qe+="+"
+		else:
+			qe+=q[i]
+
+	counter = 0
+	global allLinks
+
+	for i in range(0,1):
+		google_url = br.open("https://www.google.co.in/search?q=" + qe + "&amp;amp;start=" + str(counter))
+		search_keyword = google_url.read()
+		o = re.findall("(?P<url>https?://[^\s\"<]+)", search_keyword)
+		allLinks = uniqueListConc(allLinks,o)
+		break
+		#print len(o)
+		print "."
+		'''
+		break
+		if "rahullahoria" in search_keyword:
+			alert = "found"
+			break
+		'''
+		counter+=10
+
+	#print len(allLinks)
+	for link in allLinks:
+		page = savePage( link,0 )
+
+
 ###################################
-
-br = mechanize.Browser() #initiating a browser
-
-br.set_handle_robots(False) #ignore robots.txt
-
-br.addheaders = [("User-agent","Mozilla/5.0")] #our identity in the web
-
+allLinks = []
 q = raw_input("enter the keyword::") #keyword/keyphrase
 
-qe=""
+getGSLinks()
 
-alert = ""
-for i in range(0,len(q)):
-    if q[i] ==" ":
-        qe+="+"
-    else:
-        qe+=q[i]
-
-counter = 0
-allLinks = []
-
-for i in range(0,1):
-    google_url = br.open("https://www.google.co.in/search?q=" + qe + "&amp;amp;start=" + str(counter))
-    search_keyword = google_url.read()
-    o = re.findall("(?P<url>https?://[^\s\"<]+)", search_keyword)
-    allLinks = uniqueListConc(allLinks,o)
-    break
-    #print len(o)
-    print "."
-    '''
-    break
-    if "rahullahoria" in search_keyword:
-        alert = "found"
-        break
-    '''
-    counter+=10
-
-print len(allLinks)
-for link in allLinks:
-    page = savePage( link )
-if alert == "found":
-    print "Found at page:: ",i+1
-else:
-    print "not found"
